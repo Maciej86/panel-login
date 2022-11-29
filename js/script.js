94039f4098133d8f03bf557efcddf200fd1c01a2
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-11-29 23:26:11 +0100

    Added password visibility toggle function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 {
+  let showPassword = false;
+
   const onFormSubmit = (event) => {
     event.preventDefault();
 
@@ -6,9 +8,18 @@
     errorElement.classList.add("toggleErrorMesage");
   };
 
+  const togglePassword = () => {
+    showPassword = !showPassword;
+    const passwordElement = document.querySelector(".js-password");
+    passwordElement.setAttribute("type", showPassword ? "text" : "password");
+  };
+
   const init = () => {
     const form = document.querySelector(".js-form");
+    const togglePasswordElement = document.querySelector(".js-togglePassword");
+
     form.addEventListener("submit", onFormSubmit);
+    togglePasswordElement.addEventListener("click", togglePassword);
   };
 
   init();
